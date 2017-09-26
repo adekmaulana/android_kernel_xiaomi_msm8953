b48378c61195546bf2a2ef163267475f5c146720
Park Ju Hyung <qkrwngud825@gmail.com>
2017-09-26 16:37:08 +0900

    f2fs: rapidly GC all invalid blocks when screen goes off & plugged in
    Rapidly GC'ing all invalid blocks only takes a few minute,
    even under extreme scenario.
    
    Wait until the screen goes off and detect if the power source is plugged in.
    If it is, GC thread runs repeatedly with 1ms interval.
    
    Since the device can go to sleep even with power source plugged in,
    catch an additional wakelock.
    
    When no more GC victim block is found, GC thread sleeps for
    gc_no_gc_sleep_time(5m).
    
    With the previous ioprio patch, the impact to system responsiveness
    (e.g. waking up the screen) is minimal, if not zero.
    
    This patch is intended to optimize background GC behavior specific to Android.
    So, disable background_gc by default as well.
    
    Change-Id: I8f6d33ae646b8910654e8d311ee774f4b7756057
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1030,6 +1030,7 @@ static void f2fs_put_super(struct super_block *sb)
 
 	/* write_checkpoint can update stat informaion */
 	f2fs_destroy_stats(sbi);
+	f2fs_sbi_list_del(sbi);
 
 	/*
 	 * normally superblock is clean, so we need to release this.
@@ -1371,7 +1372,6 @@ static void default_options(struct f2fs_sb_info *sbi)
 	F2FS_OPTION(sbi).test_dummy_encryption = false;
 	sbi->readdir_ra = 1;
 
-	set_opt(sbi, BG_GC);
 	set_opt(sbi, INLINE_XATTR);
 	set_opt(sbi, INLINE_DATA);
 	set_opt(sbi, INLINE_DENTRY);
@@ -2921,6 +2921,8 @@ try_onemore:
 	if (err)
 		goto free_node_inode;
 
+	f2fs_sbi_list_add(sbi);
+
 	/* read root inode and dentry */
 	root = f2fs_iget(sb, F2FS_ROOT_INO(sbi));
 	if (IS_ERR(root)) {
@@ -3058,6 +3060,7 @@ free_node_inode:
 	release_ino_entry(sbi, true);
 	truncate_inode_pages_final(NODE_MAPPING(sbi));
 	iput(sbi->node_inode);
+	f2fs_sbi_list_del(sbi);
 free_nm:
 	destroy_node_manager(sbi);
 free_sm:
