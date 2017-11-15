b9211e464c2f0b7ede329f79812a68390ca6ecf2
Sultanxda <sultanxda@gmail.com>
2017-11-15 08:47:23 +0700

    clk: msm: clock-cpu-8953: Use CLKFLAG_NO_RATE_CACHE for perfcl_hf_mux
    The performance cluster can have frequency change requests ignored when
    it comes online without having this flag set. This can result in the perfcl
    running at its default frequency instead of the frequency that the CPU
    governor wants.
    
    Change-Id: If51635856ae44d51bb4283ee6f41735cc6220ebb
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/clk/msm/clock-cpu-8953.c
+++ drivers/clk/msm/clock-cpu-8953.c
@@ -32,6 +32,7 @@
 #include <linux/suspend.h>
 #include <linux/regulator/rpm-smd-regulator.h>
 #include <linux/uaccess.h>
+#include <linux/clk/msm-clk-provider.h>
 #include <soc/qcom/clock-local2.h>
 #include <soc/qcom/clock-pll.h>
 
@@ -398,6 +399,7 @@ static struct mux_clk cpu_debug_ter_mux = {
 	.base = &virt_bases[APCS0_DBG_BASE],
 	.c = {
 		.dbg_name = "cpu_debug_ter_mux",
+		.flags = CLKFLAG_NO_RATE_CACHE,
 		.ops = &clk_ops_gen_mux,
 		CLK_INIT(cpu_debug_ter_mux.c),
 	},
