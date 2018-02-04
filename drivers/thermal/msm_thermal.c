594ac40be7ec81a7a6b59797a0f6b24d54b170a4
Adek Maulana <adekboreanaz17@gmail.com>
2018-02-04 08:10:51 +0700

    Revert "msm: thermal: add a module param to change the thermal throttle temperature point to userspace"
    This reverts commit bc289ed672984119e3cd81edb43601f9b4652f1d.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -117,9 +117,6 @@
 		} \
 	} while (0)
 
-unsigned int temp_threshold = 80; // 80 for MIDO
-module_param(temp_threshold, int, 0755);
-
 static struct msm_thermal_data msm_thermal_info;
 static struct delayed_work check_temp_work, retry_hotplug_work;
 static bool core_control_enabled;
@@ -3412,14 +3409,14 @@ static void do_freq_control(long temp)
 	if (!freq_table_get)
 		return;
 
-	if (temp >= temp_threshold) {
+	if (temp >= msm_thermal_info.limit_temp_degC) {
 		if (limit_idx == limit_idx_low)
 			return;
 
 		limit_idx -= msm_thermal_info.bootup_freq_step;
 		if (limit_idx < limit_idx_low)
 			limit_idx = limit_idx_low;
-	} else if (temp < temp_threshold -
+	} else if (temp < msm_thermal_info.limit_temp_degC -
 		 msm_thermal_info.temp_hysteresis_degC) {
 		if (limit_idx == limit_idx_high)
 			return;
@@ -3554,16 +3551,12 @@ static int hotplug_notify(enum thermal_trip_type type, int temp, void *data)
 		return 0;
 	switch (type) {
 	case THERMAL_TRIP_CONFIGURABLE_HI:
-		if (!(cpu_node->offline)) {
-			pr_info("%s reached HI temp threshold: %d\n", cpu_node->sensor_type, temp);
+		if (!(cpu_node->offline))
 			cpu_node->offline = 1;
-		}
 		break;
 	case THERMAL_TRIP_CONFIGURABLE_LOW:
-		if (cpu_node->offline) {
-			pr_info("%s reached LOW temp threshold: %d\n", cpu_node->sensor_type, temp);
+		if (cpu_node->offline)
 			cpu_node->offline = 0;
-		}
 		break;
 	default:
 		break;
