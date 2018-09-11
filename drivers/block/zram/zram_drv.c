6aadefcfca614ac786370ae1fe8514f90bb07d90
Bish Project <adek@bishproject.net>
2018-09-11 14:09:12 +0700

    mido: change zstd to lz4 algorithm zram
    Change-Id: I15837e7ae823048ad495fc8c33b7e44be4e9cc89

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -40,8 +40,8 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-#if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-	static const char *default_compressor = "zstd";
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4)
+	static const char *default_compressor = "lz4";
 #else
 	static const char *default_compressor = "lzo";
 #endif
