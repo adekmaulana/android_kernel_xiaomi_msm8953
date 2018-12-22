dcfc238e1242451a86c32ab54bde4b62c3c54d0e
Adek Maulana <adekzmaulana@gmail.com>
2018-12-22 22:23:23 +0700

    Revert "BACKPORT: partial: mm, oom_reaper: do not mmput synchronously from the oom reaper context"
    This reverts commit d2dbce5bc3c550db081efb6ba1791cee9f2dac19.

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -14,7 +14,6 @@
 #include <linux/page-debug-flags.h>
 #include <linux/uprobes.h>
 #include <linux/page-flags-layout.h>
-#include <linux/workqueue.h>
 #include <asm/page.h>
 #include <asm/mmu.h>
 
@@ -473,7 +472,6 @@ struct mm_struct {
 #ifdef CONFIG_MSM_APP_SETTINGS
 	int app_setting;
 #endif
-	struct work_struct async_put_work;
 
 };
 
